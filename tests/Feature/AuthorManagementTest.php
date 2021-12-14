039fe6b87c1dde9557746c9a67e61e112a50d02f
Toky 3AT <tokyarisily@gmail.com>
2021-12-14 15:12:05 +0100

    wip for authors and change route from author to authors

--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -17,16 +17,38 @@ class AuthorManagementTest extends TestCase
 	{
 		// $this->withoutExceptionHandling();
 
-		$this->post('/author',[
-			'name' => 'Author Name',
-			'dob' => '03/16/2014',
-		]);
+		$this->post('/authors', $this->data());
 
 		$author = Author::all();
 
 		$this->assertCount(1, $author);	
 		$this->assertInstanceOf(Carbon::class, $author->first()->dob);	
 		$this->assertEquals('2014/16/03', $author->first()->dob->format('Y/d/m'));
+	}
+
+	/** @test */
+	public function a_name_is_required()
+	{
+		// $this->withoutExceptionHandling();
+		$response = $this->post('/authors', array_merge($this->data(), ['name' => '']));
+		$response->assertSessionHasErrors(keys: 'name');		
+	}
+
+	/** @test */
+	public function a_dob_is_required()
+	{
+		// $this->withoutExceptionHandling();
+		$response = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+		$response->assertSessionHasErrors(keys: 'dob');		
+	}
+
+
+	private function data()
+	{
+		return [
+			'name' => 'Author Name',
+			'dob' => '03/16/2014',
+		];
 	}
     
 }
