8fe99803bf5d78067dfc747891ac0093ffe17e0f
Toky 3AT <tokyarisily@gmail.com>
2021-12-14 12:11:08 +0100

    Add Breeze to the app

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\CheckinBookController;
+use App\Http\Controllers\CheckoutBookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,12 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::post('/books', [BooksController::class, 'store']);
 Route::patch('/books/{book}', [BooksController::class, 'update']);
 Route::delete('/books/{book}', [BooksController::class, 'destroy']);
 
 Route::post('/author', [AuthorController::class, 'store']);
+
+Route::post('/checkout/{book}', [CheckoutBookController::class, 'store']);
+Route::post('/checkin/{book}', [CheckinBookController::class, 'store']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
